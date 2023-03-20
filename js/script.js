e923407459f946d22b231831872074d40f6bb4b5
Alessio Libbi <alex.libbi@gmail.com>
2023-03-20 17:57:39 +0100

    Add: block sistem

--- js/script.js
+++ js/script.js
@@ -11,6 +11,7 @@ let value = document.getElementById("modalita")
 // Per ogni numero generato genero le caselle della griglia
 button.addEventListener("click", function () {
     grid.innerHTML = ""
+    grid.classList.remove("disabled")
     if (value.value === "easy") {
         numberOfSquares = 100;
 
@@ -19,6 +20,7 @@ button.addEventListener("click", function () {
     } else {
         numberOfSquares = 49;
     }
+
     for (let i = 1; i <= numberOfSquares; i++) {
         const currentNumber = i;
         const newItem = generateGridItem(currentNumber);
@@ -28,45 +30,54 @@ button.addEventListener("click", function () {
 
 
 
+    function generateGridItem(text) {
+        const newSquare = document.createElement("div");
+        let modalities = value.value
+        newSquare.classList.add("grid-item", modalities);
+        newSquare.innerHTML = `<span>${text}</span>`;
+        return newSquare;
+    
+    }
+    // dato il numero massimo di click possibili (max number- bomb number) una volta raggiunto stamo hai visto
+    
+    function handleItemClick() {
+        const clickedNumber = parseInt(this.querySelector("span").textContent);
+        this.classList.add("green");
+        this.classList.add("disabled")
+        console.log(clickedNumber);
+        
+        // check se clickedNumber è presente tra i numeri casuali
+        
+        if (numeriCasuali.includes(clickedNumber)) {
+            console.log("hai perso");
+            this.classList.add("red")
+            grid.classList.add("disabled")
+        } 
+        
+    }
+    
+    // creare la funzione per numero casuale
+    numeriCasuali = generaNumeriCasualiNonRipetuti(1, 100, 16);
+    function generaNumeriCasualiNonRipetuti(min, max, lunghezza) {
+        const numeriCasuali = [];
+    
+        while (numeriCasuali.length < lunghezza) {
+            const numeroCasuale = Math.floor(Math.random() * (max - min + 1)) + min;
+    
+            if (numeriCasuali.indexOf(numeroCasuale) === -1) {
+                numeriCasuali.push(numeroCasuale);
+            }
+        }
+    
+        return numeriCasuali;
+    }
 })
 
 //////////////////////////////////////////////
 // FUNCTIONS
 // DOM related functions
 
-function generateGridItem(text) {
-    const newSquare = document.createElement("div");
-    let modalities = value.value
-    newSquare.classList.add("grid-item", modalities);
-    newSquare.innerHTML = `<span>${text}</span>`;
-    return newSquare;
-
-}
-
-function handleItemClick() {
-    const clickedNumber = parseInt(this.querySelector("span").textContent);
-    console.log(clickedNumber);
-    this.classList.add("green");
-    // if clickedNumer = arraY RANDOM NUMBER = PERSO /STOP
-
-}
-
-// creare la funzione per numero casuale
-numeriCasuali = generaNumeriCasualiNonRipetuti(1, 100, 16);
-function generaNumeriCasualiNonRipetuti(min, max, lunghezza) {
-    const numeriCasuali = [];
-
-    while (numeriCasuali.length < lunghezza) {
-        const numeroCasuale = Math.floor(Math.random() * (max - min + 1)) + min;
-
-        if (numeriCasuali.indexOf(numeroCasuale) === -1) {
-            numeriCasuali.push(numeroCasuale);
-        }
-    }
-
-    return numeriCasuali;
-}
 
 // Esempio d'uso della funzione
 
-console.log(numeriCasuali);
+
